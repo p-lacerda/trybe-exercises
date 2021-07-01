5ab0efaf512c1ffddd2397312b7a0df610aaa548
p-lacerda <xcloudgameemail@gmail.com>
2021-06-30 22:04:59 -0300

    Exercicios feitos

---
+++ fundamentals/block_5/day_1/para_fixar/parte3/script.js
@@ -0,0 +1,9 @@
+document.querySelector("#header-container").style = 'color: green'
+
+document.querySelector(".emergency-tasks h3").style = "color: red"
+
+document.querySelector("#footer-container").style = 'color: green'
+
+document.querySelectorAll('.emergency-tasks').style = "color: black"
+
+document.querySelector('.no-emergency-tasks').style = "background-color: blue"
