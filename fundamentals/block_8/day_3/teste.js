be71c7030274f133eef92c8343512601655f33b2
p-lacerda <xcloudgameemail@gmail.com>
2021-07-31 00:44:47 -0300

    Adiciona arquivos do dia 3, HOF

---
+++ fundamentals/block_8/day_3/teste.js
@@ -0,0 +1,24 @@
+const numbers = [2, 20, 30, 5, 97, 93, 77, 199, 198];
+const letras = ['Paulo', 'Pedro', 'Pericles', 'Parseu', 'John'];
+
+// 1
+
+// const verifyIsEven = (number) => number % 2 === 0;
+
+// const isEven = numbers.filter(verifyIsEven);
+
+// console.log(isEven)
+
+
+// 2
+
+// const verifyLetter = (name) => name[0] === 'P';
+
+// const letterP = letras.filter(verifyLetter)
+
+// console.log(letterP)
+
+
+// Without function 
+// const isEven = numbers.filter((number) => number % 2 === 0)
+// console.log(isEven)
