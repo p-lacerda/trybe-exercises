094175493aed3df5b419aeba8d0da98b0bdc9737
p-lacerda <xcloudgameemail@gmail.com>
2021-09-05 17:01:26 -0300

    Adiciona arquivos feitos no dia 2

---
+++ front-end/block_12/day_2/part2/my-forms/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Forms from './Forms'
+
+class App extends React.Component {
+
+  render () {
+    return (
+      <>
+      <Forms />
+      </>
+      );
+  }
+}
+
+export default App;
